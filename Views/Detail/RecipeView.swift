ebd766760f933ad456e1ff9c6c0e026ba00bbe31
ema <130402584+emazech@users.noreply.github.com>
2024-02-29 07:10:16 +0100

    add recipe card and list as a component, filter recipe by category, add new recipe

---
+++ Views/Detail/RecipeView.swift
@@ -0,0 +1,66 @@
+import SwiftUI
+
+struct RecipeView: View {
+    var recipe: Recipe
+    
+    var body: some View {
+        ScrollView {
+            AsyncImage(url: URL(string: recipe.image)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .frame(minWidth: .infinity, maxWidth: .infinity)
+            }
+            .frame(height: 320)
+            .background(
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.gray.opacity(0.3), Color.gray]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+            
+            VStack(spacing: 30) {
+                Text(recipe.name)
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 50)
+                
+                VStack(alignment: .leading, spacing: 30) {
+                    if !recipe.description.isEmpty {
+                        Text(recipe.description)
+                    }
+                    if !recipe.ingridients.isEmpty {
+                    VStack(alignment: .leading, spacing: 20) {
+                    
+                            Text("Ingredients: ").font(.headline)
+                            
+                            Text(recipe.ingridients)
+                        }
+                    }
+                    if !recipe.directions.isEmpty {
+                    VStack(alignment: .leading, spacing: 20) {
+           
+                            Text("Direction: ").font(.headline)
+                            
+                            Text(recipe.directions)
+                        }
+                    }
+                }
+            }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading).padding(.horizontal)
+        }
+        .ignoresSafeArea(.container, edges: .top)
+    }
+}
+
+struct RecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeView(recipe: Recipe.all[0])
+    }
+}
